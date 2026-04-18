1870c1392f27bc7fa86c0f3b7c6f65c7aa8d3ffb
RickyDas999 <116985003+RickyDas999@users.noreply.github.com>
2026-04-18 18:03:35 -0500

    Hook up UI to API to display events

--- client/src/pages/ExplorePage.tsx
+++ client/src/pages/ExplorePage.tsx
@@ -1,13 +1,39 @@
+import {useState, useEffect} from 'react';
+
+type EventItem = {
+  id: number;
+  title: string;
+  category: string;
+  venue: string;
+  city: string;
+  event_date: string;
+  price: string;
+}
+
 function ExplorePage() {
+
+  const [events, setEvents] = useState<EventItem[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/events")
+      .then(res => res.json())
+      .then(data => setEvents(data))
+  }, [])
+
   return (
     <main>
       <section>
         <p>Encore</p>
         <h1>Discover events worth the trip.</h1>
-        <p>
-          Start by wiring the Explore page to the backend events API and add
-          filtering for city, category, and date.
-        </p>
+        <p>{events.length} events loaded... </p>
+
+        {events.map(event => (
+          <div key = {event.id}> 
+            {event.title} - {event.city} - {new Date(event.event_date).toLocaleDateString()} - ${event.price}
+          </div>
+        )
+        )}
+
       </section>
     </main>
   );
