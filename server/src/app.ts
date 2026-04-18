2253c459ff050f9221ac9a2d55a8c8463309370d
RickyDas999 <116985003+RickyDas999@users.noreply.github.com>
2026-04-17 19:10:44 -0500

    Set up initial endpoint

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,13 +1,16 @@
 import cors from "cors";
 import express from "express";
+import pool from "./db/pool.js";
+
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
-app.get("/health", (_request, response) => {
-  response.json({ status: "ok" });
+
+app.get("/events", async (req, res) => {
+  const { rows } = await pool.query("SELECT * FROM events");
+  res.json(rows);
 });
 
 export default app;
