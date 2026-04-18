d165873d6fc8c9d6a44c29c3b32e798c1ba008bf
RickyDas999 <116985003+RickyDas999@users.noreply.github.com>
2026-04-17 20:38:33 -0500

    Add all get requests

--- server/src/app.ts
+++ server/src/app.ts
@@ -13,5 +13,19 @@ app.get("/events", async (req, res) => {
   res.json(rows);
 });
 
-export default app;
+app.get("/users/:id/favorites", async(req, res) => {
+  const { rows } = await pool.query("SELECT events.* FROM favorites JOIN events ON favorites.event_id = events.id WHERE favorites.user_id = $1", [req.params.id]);
+  res.json(rows);
+});
 
+app.get("/users/:id/trips", async(req, res) => {
+  const { rows } = await pool.query("SELECT trips.* FROM trips WHERE trips.user_id = $1", [req.params.id]);
+  res.json(rows);
+})
+
+app.get("/trips/:id/", async(req, res) => {
+  const { rows } = await pool.query("SELECT trips.title AS trip_title, events.* FROM trip_events JOIN events ON trip_events.event_id = events.id JOIN trips ON trip_events.trip_id=trips.id WHERE trip_events.trip_id = $1", [req.params.id])
+  res.json(rows);
+})
+
+export default app;
